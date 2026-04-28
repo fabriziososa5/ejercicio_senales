4474ac0b8405de388205f58fd79d52607c426ba6
fabriziososa5 <fabriziososa5.n@gmail.com>
2026-04-28 10:14:52 -0300

    Commit 7: conectar manualmente
    Conectar manualmente (con .connect) la escena “Menu” con las instancias de “RedGenerator” y “VioletGenerator”. La conexión debe ser mediante la señal “button_down” y debe llamar a la función “count_new_instance”

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -3,8 +3,15 @@ extends Node2D
 const INSTANCES_TEXT = "Cantidad de instancias: "
 var instancesCount = 0
 
+@onready var count_label = $CountLabel
+@onready var red_button = $RedGenerator/TextureButton
+@onready var violet_button = $VioletGenerator/TextureButton
+
 func _ready():
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+
+	red_button.button_down.connect(count_new_instance)
+	violet_button.button_down.connect(count_new_instance)
 
 func count_new_instance():
 	instancesCount += 1
