09703af1480846f3a33f6e5b6eb0344f531d4441
fabriziososa5 <fabriziososa5.n@gmail.com>
2026-04-28 10:16:31 -0300

    Commit 8: "delete"
    En el script de “Menu”, crear una función llamada “delete” donde al ser ejecutada, vuelva a poner en “0” el contador “count_new_instance” y actualice el texto de “CountLabel”.

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -16,3 +16,7 @@ func _ready():
 func count_new_instance():
 	instancesCount += 1
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	
+func delete():
+	instancesCount = 0
+	count_label.text = INSTANCES_TEXT + str(instancesCount)
